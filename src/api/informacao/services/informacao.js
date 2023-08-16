9798937b19be64af4a8df74c182232bc33b3dd08
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2023-08-16 01:49:29 -0300

    Task/create content types (#1)
    * create pagina content type
    
    * create banner content type
    
    * create noticias content type
    
    * add fields to noticia content type
    
    * create service content type
    
    * create informacao single type
    
    * create parceiros collection type
    
    * fix collections type

---
+++ src/api/informacao/services/informacao.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * informacao service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::informacao.informacao');
---
+++ src/api/parceiro/services/parceiro.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * parceiro service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::parceiro.parceiro');
