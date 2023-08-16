b2571884d4d5e4ee569488f5c683772de8495b15
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2023-08-16 01:55:14 -0300

    Develop (#2)
    * initial base strapi
    
    * Task/create content types (#1)
    
    * create pagina content type
    
    * create banner content type
    
    * create noticias content type
    
    * add fields to noticia content type
    
    * create service content type
    
    * create informacao single type
    
    * create parceiros collection type
    
    * fix collections type

---
+++ src/api/parceiro/controllers/parceiro.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * parceiro controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::parceiro.parceiro');
