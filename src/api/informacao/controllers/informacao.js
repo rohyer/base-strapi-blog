bf7dd3a69592e8da45f984540b209977ff03e56f
Rohyer <guilhermerl.dev@gmail.com>
2023-08-16 01:26:56 -0300

    create informacao single type

---
+++ src/api/informacao/controllers/informacao.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * informacao controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::informacao.informacao');
