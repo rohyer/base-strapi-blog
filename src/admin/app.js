c06c4bff76af7efd0d702696b9a763a508fa8396
Rohyer <guilhermerl.dev@gmail.com>
2023-09-03 15:09:11 -0300

    change dashboard language to portugues br

---
+++ src/admin/app.js
@@ -0,0 +1,15 @@
+export default {
+  config: {
+    locales: ['pt-BR'],
+    translations: {
+      pt: {
+        'Auth.form.email.label': 'test',
+        Users: 'Utilisateurs',
+        City: 'CITY (Brazil)',
+        // Customize the label of the Content Manager table.
+        Id: 'ID brazil',
+      },
+    },
+  },
+  bootstrap() {},
+};
